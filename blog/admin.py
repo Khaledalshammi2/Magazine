3b6ed2fbc508531bdbd32acdf910b6128464c43b
Khaledalshammi2 <khaled.alshammi20@gmail.com>
2023-02-27 11:44:03 +0300

    added author model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
+from .models import Blog, Magazine
 
-# Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Author(models.Model):
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    profile_image = models.ImageField(upload_to='profile_images/')
+
+
 class Magazine(models.Model):
-    name = models.CharField(max_length=100)
-    publisher = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     publication_date = models.DateField(auto_now_add=True)
     description = models.TextField()
 
     def __str__(self) -> str:
-        return self.name
+        return self.title
 
 
 class Blog(models.Model):
@@ -19,7 +23,7 @@ class Blog(models.Model):
         ('p', 'Published'),
         ('w', 'Withdrawn'),
     ]
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     content = models.TextField()
     magazine = models.ForeignKey(Magazine, on_delete=models.CASCADE)
